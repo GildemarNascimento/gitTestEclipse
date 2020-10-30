acc1df75f7a107204ef128565d0bca4636ac9b07
GildemarNascimento <gildemargg@hotmail.com>
2020-10-30 10:55:33 -0300

    Criação da classe Actor

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,11 +2,14 @@ package application;
 
 import java.awt.print.Book;
 
+import entities.Actor;
+
 public class Program {
 
 	public static void main(String[] args) {
 	
 		Book l = new Book();
+		Actor a = new Actor();
 	}
 
 }
